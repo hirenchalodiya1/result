a9460b50a41dfce9fb605bb21443a3dbd466f5c0
Hiren Chalodiya <chalodiya.1@iitj.ac.in>
2020-06-09 08:24:23 +0530

    Project till now

--- excel_handling.py
+++ excel_handling.py
@@ -1,28 +1,70 @@
+# read file
 from xlrd import open_workbook
-from analyse_12science import analyse_12science
-from xlutils.copy import copy
-from takehtml import make_html
+# download file
+from takehtml import make_html, get_url
+import requests
+# analyse file
+from analyse_12science import analyse_12science, analyse_10
+# write file
+import xlsxwriter
 
-excel_file = 'one_class.xlsx'
+# import file name
+excel_file = 'input/SSC Marks Entry 2019-20.xlsx'
 try:
     readworkbook = open_workbook(excel_file, on_demand=True)
+    workbook = xlsxwriter.Workbook('output/result.xlsx')
 except:
     print('%s file not found' % excel_file)
-
-writeworkbook = copy(readworkbook)
+worksheet = workbook.add_worksheet()
+# writeworkbook = copy(readworkbook)
 i=0
-for sheet in readworkbook.sheets():
-    writesheet = writeworkbook.get_sheet(i)
-    for row in range(sheet.nrows)[1:]:
-        rolln = sheet.cell(row, 0).value
-        roll = 'B' + str(rolln)
-        roll = str(roll)[:7]
-        make_html(roll)
-        analyse_value = analyse_12science(roll)
-        for col in range(len(analyse_value)):
-            writesheet.write(row, col + 1, analyse_value[col])
-        i=i+1
+rl= []
+indecies = {
+    'seat': 'A',
+    'name': 'B',
+    'guj_board': 'C',
+    'guj_school': 'D',
+    'guj_total': 'E',
+    'ss_board': 'F',
+    'ss_school': 'G',
+    'ss_total': 'H',
+    'sci_board': 'I',
+    'sci_school': 'J',
+    'sci_total': 'K',
+    'san_board': 'L',
+    'san_school': 'M',
+    'san_total': 'N',
+    'eng_board': 'O',
+    'eng_school': 'P',
+    'eng_total': 'Q',
+    'math_board': 'R',
+    'math_school': 'S',
+    'math_total': 'T',
+    'total': 'U',
+    'grade': 'V',
+    'percentile': 'W'
+}
 
-writeworkbook.save(excel_file)
+for sheet in readworkbook.sheets():
+    # writesheet = writebook.sheets()[0]
+    for row in range(1, sheet.nrows):
+        roll = sheet.cell(row, 4).value
+        roll = roll[0] + roll[2:]
+        rl.append(roll)
+        make_html(roll)
+        # preparing url
+        # url = 'http://gseb.org/522lacigam/sci/{roll[:3]}/{roll[3:5]}/{roll}.html'
+        # res = requests.get(url)
+        print(f'{roll} looked up...')
+        # content = res.text
+        with open(f'{roll}.html', 'r') as f:
+            content = f.read()
+        ret = analyse_10(content)
+        for key, value in ret.items():
+            c_no = indecies.get(key, None)
+            if c_no:
+                worksheet.write(f'{c_no}{row+1}', value)
+
+workbook.close()
 readworkbook.release_resources()
-del readworkbook,sheet,i,writeworkbook,writesheet,analyse_value,roll
+# del readworkbook,sheet,i,writeworkbook,writesheet,analyse_value,roll
